2991a4bcd1162fcb89939aa672e59b19aa69111e
花井直人 <nao@hanamaru.games>
2025-10-14 17:46:23 +0900

    カメラ上下操作を追加

--- Action3D/Src/Camera.cpp
+++ Action3D/Src/Camera.cpp
@@ -18,11 +18,16 @@ Camera::~Camera()
 void Camera::Update()
 {
 	transform.rotation.y += RStickX() * 6.0f * DegToRad;
+	transform.rotation.x += RStickY() * 6.0f * DegToRad;
 
-	MATRIX4X4 mat = XMMatrixRotationY(
+	MATRIX4X4 matY = XMMatrixRotationY(
 		transform.rotation.y);
-	VECTOR3 eye = VECTOR3(0, 2, -5) * mat;
-	VECTOR3 look = VECTOR3(0, 1, 0);
+	MATRIX4X4 matX = XMMatrixRotationX(
+		transform.rotation.x);
+
+	VECTOR3 eye = VECTOR3(0, 2, -5) * matX * matY 
+			+ VECTOR3(0, 1.5f, 0);
+	VECTOR3 look = VECTOR3(0, 2, 0);
 	GameDevice()->m_vEyePt = eye; // カメラ座標
 	GameDevice()->m_vLookatPt = look; // 注視点
 	GameDevice()->m_mView = XMMatrixLookAtLH(     // ビューマトリックス
@@ -39,12 +44,28 @@ float Camera::RStickX()
 	if (GameDevice()->m_pDI->CheckKey(KD_DAT, DIK_LEFT)) {
 		return -1.0f;
 	}
-	if (GameDevice()->m_pDI->GetJoyNum()==0)
+	if (GameDevice()->m_pDI->GetJoyNum() == 0)
 		return 0.0f;
 	auto inp = GameDevice()->m_pDI->GetJoyState();
 	float x = inp.lRx;
 	x = x / 65535.0f * 2.0f - 1.0f;
 	return x;
+}
+
+float Camera::RStickY()
+{
+	if (GameDevice()->m_pDI->CheckKey(KD_DAT, DIK_DOWN)) {
+		return 1.0f;
+	}
+	if (GameDevice()->m_pDI->CheckKey(KD_DAT, DIK_UP)) {
+		return -1.0f;
+	}
+	if (GameDevice()->m_pDI->GetJoyNum() == 0)
+		return 0.0f;
+	auto inp = GameDevice()->m_pDI->GetJoyState();
+	float y = inp.lRy;
+	y = y / 65535.0f * 2.0f - 1.0f;
+	return y;
 }
 
 
