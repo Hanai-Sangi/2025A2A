e9e56566bc778e05c58af098f48c50e9a8647715
花井直人 <nao@hanamaru.games>
2025-10-22 20:41:28 +0900

    左スティックでキャラ移動できました

--- Action3D/Src/Camera.cpp
+++ Action3D/Src/Camera.cpp
@@ -1,31 +1,44 @@
 #include "Camera.h"
 
+const VECTOR3 EyeBase = VECTOR3(0, 0, -5.2f);
+const float AngleBase = 30.0f * DegToRad;
+
 Camera::Camera()
 {
-	VECTOR3 eye = VECTOR3(0, 2, -5);
-	VECTOR3 look = VECTOR3(0, 1, 0);
-	GameDevice()->m_vEyePt = eye; // カメラ座標
-	GameDevice()->m_vLookatPt = look; // 注視点
-	GameDevice()->m_mView = XMMatrixLookAtLH(     // ビューマトリックス
-		eye, look,
-		VECTOR3(0, 1, 0));
+	transform.rotation.x = AngleBase;
+
+	//VECTOR3 eye = VECTOR3(0, 2, -5);
+	//VECTOR3 look = VECTOR3(0, 1, 0);
+	//GameDevice()->m_vEyePt = eye; // カメラ座標
+	//GameDevice()->m_vLookatPt = look; // 注視点
+	//GameDevice()->m_mView = XMMatrixLookAtLH(     // ビューマトリックス
+	//	eye, look,
+	//	VECTOR3(0, 1, 0));
 }
 
 Camera::~Camera()
 {
 }
 
+float clampMe(float& d, float minV, float maxV)
+{
+	if (d<minV) d = minV;
+	if (d>maxV) d = maxV;
+	return d;
+}
+
 void Camera::Update()
 {
 	transform.rotation.y += RStickX() * 6.0f * DegToRad;
 	transform.rotation.x += RStickY() * 6.0f * DegToRad;
+	clampMe(transform.rotation.x, -30*DegToRad, 85*DegToRad);
 
 	MATRIX4X4 matY = XMMatrixRotationY(
 		transform.rotation.y);
 	MATRIX4X4 matX = XMMatrixRotationX(
 		transform.rotation.x);
 
-	VECTOR3 eye = VECTOR3(0, 2, -5) * matX * matY 
+	VECTOR3 eye = EyeBase * matX * matY 
 			+ VECTOR3(0, 1.5f, 0);
 	VECTOR3 look = VECTOR3(0, 2, 0);
 	GameDevice()->m_vEyePt = eye; // カメラ座標
--- Action3D/Src/Player.cpp
+++ Action3D/Src/Player.cpp
@@ -28,4 +28,46 @@ void Player::Update()
 	ImGui::End();
 
 	animator->Update();
+
+	VECTOR2 stick = LStickVec();
+	VECTOR3 camVec = GameDevice()->m_vLookatPt -
+							GameDevice()->m_vEyePt;
+	float camAng = atan2f(camVec.x, camVec.z); // ƒJƒƒ‰‚ÌŠp“x
+	VECTOR3 velocity = VECTOR3(stick.x, 0, -stick.y);
+	if (magnitude(velocity) > 0) {
+		velocity = velocity * XMMatrixRotationY(camAng);
+		transform.rotation.y = atan2f(velocity.x, velocity.z);
+		transform.position += velocity * 0.05f;
+	}
+	//ƒƒ‚
+	XMMATRIX mat = XMMatrixRotationY(transform.rotation.y);
+	VECTOR3 front = VECTOR3(0, 0, 1) * mat; // ³–Ê
+	VECTOR3 right = VECTOR3(1, 0, 0) * mat; // ‰E
+	VECTOR3 up = VECTOR3(0, 1, 0) * mat; // ã
+}
+
+VECTOR2 Player::LStickVec()
+{
+	auto inp = GameDevice()->m_pDI->GetJoyState();
+	float x = inp.lX;
+	float y = inp.lY;
+	x = x / 1000.0f;
+	y = y / 1000.0f;
+	if (GameDevice()->m_pDI->CheckKey(KD_DAT, DIK_W)) {
+		y = -1.0f;
+	}
+	if (GameDevice()->m_pDI->CheckKey(KD_DAT, DIK_S)) {
+		y = 1.0f;
+	}
+	if (GameDevice()->m_pDI->CheckKey(KD_DAT, DIK_D)) {
+		x = 1.0f;
+	}
+	if (GameDevice()->m_pDI->CheckKey(KD_DAT, DIK_A)) {
+		x = -1.0f;
+	}
+	VECTOR2 ret = VECTOR2(x, y);
+	if (magnitude(ret) > 1.0f) { // ’·‚³‚ª‚P‚ð’´‚¦‚Ä‚é‚Ì‚Å
+		ret = normalize(ret); // ’·‚³‚ð‚P‚É‚·‚é
+	}
+	return ret;
 }
